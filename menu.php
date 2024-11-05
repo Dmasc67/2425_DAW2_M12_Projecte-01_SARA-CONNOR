3918abfcc7a26978a0a75ac1cb6e794e0c7a2ee8
PanDyyG <angel.camru@gmail.com>
2024-11-05 20:27:04 +0100

    menu con css, falta probar

--- menu.php
+++ menu.php
@@ -1,16 +1,49 @@
+<?php
+session_start();
+if (!isset($_SESSION['id'])) {
+    header("Location: ./index.php");
+    exit;
+} else if (isset($_GET['logout'])) {
+    session_destroy();
+    header("Location: ./index.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RICK DECKARD - HOME</title>
-    <link rel="stylesheet" href="./css/home.css">
-    <link rel="shortcut icon" href="./img/LOGORICK.png" type="image/x-icon">
+    <title>Menu</title>
+    <link rel="stylesheet" href="./css/menu.css">
+    <link rel="shortcut icon" href="./img/logo.png" type="image/x-icon">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="./js/popup.js" defer></script>
     <style>
+        /* Estilo para el botón en la esquina superior derecha */
+        .image-item {
+            position: relative;
+            display: inline-block;
+        }
+
+        .image-item .btn-top-right {
+            position: absolute;
+            top: 0;
+            right: 0;
+            z-index: 1;
+            /* Ajusta el z-index para que esté sobre la imagen */
+            background-color: #28a745;
+            /* Color de fondo del botón */
+            color: #fff;
+            /* Color del texto del botón */
+            border: none;
+            padding: 5px 10px;
+            cursor: pointer;
+            font-size: 14px;
+        }
     </style>
 </head>
 
@@ -19,13 +52,14 @@
         <div class="container">
             <div>
                 <a class="navbar-brand " href="#">
-                    <img src="./img/LOGORICK _Blanco.png" alt="" width="100" height="90">
-                   </a>
+                    <img src="./img/logo.png" alt="" width="100" height="90">
+                     echo "<a href='./registro.php'><button class='atrasboton'><img class='atrasimg' src='./img/libro.png' alt=''></button></a>";
+                    ?> </a>
             </div>
             <div class="saludo">
-                <b style="color:white">¡Bienvenido al portal, !</b>
+                <b style="color:white">¡Bienvenido al portal, <?php echo $_SESSION['Usuario']; ?>!</b>
             </div>
-            <a href="./inc/salir.php"><button class="logoutboton"><img class="logoutimg" src="./img/LOGOUT.png" alt=""></button></a>
+            <a href="./inc/salir.php"><button class="logoutboton"><img class="logoutimg" src="./img/logout.webp" alt=""></button></a>
         </div>
     </nav>
     <!------------FIN BARRA DE NAVEGACION--------------------->
@@ -62,7 +96,8 @@
         </div>
 
     </div>
-
+    <script src="./js/imagen.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 </body>
 
 </html>
