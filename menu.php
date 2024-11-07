b6ed01680273d47333a213eae93b0388e7b3e510
Dmasc67 <sergi.masip2004@gmail.com>
2024-11-07 17:24:01 +0100

    sweetalert don again :D

--- menu.php
+++ menu.php
@@ -19,7 +19,7 @@ if (!isset($_SESSION['Usuario'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body>
+<body data-usuario="<?php echo htmlspecialchars($_SESSION['Usuario'], ENT_QUOTES, 'UTF-8'); ?>">
 <div class="container">
     <nav class="navegacion">
         <!-- Sección izquierda con el logo grande y el ícono adicional más pequeño -->
@@ -57,7 +57,7 @@ if (!isset($_SESSION['Usuario'])) {
     </section>
 </div>
     
-    <script src="./js/imagen.js"></script>
+    <script src="./js/sweetalert.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 </body>
 
